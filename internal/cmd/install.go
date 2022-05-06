a4e724c26e10fa522770d0dc8e859d2c974156d0
anoriqq <shota.yoshikawa@anoriqq.com>
2022-05-06 16:25:43 +0900

    feat: install scriptを実行できるように

--- internal/cmd/install.go
+++ internal/cmd/install.go
@@ -1,6 +1,13 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
 
 var installCmd = &cobra.Command{
 	Use:   "install",
@@ -12,6 +19,45 @@ func init() {
 	rootCmd.AddCommand(installCmd)
 }
 
-func installRun(_ *cobra.Command, _ []string) error {
+func installRun(_ *cobra.Command, args []string) error {
+	pkgName, err := getPkgName(args)
+	if err != nil {
+		return err
+	}
+
+	installScriptPath, err := getInstallScriptPaht(pkgName)
+	if err != nil {
+		return err
+	}
+
+	return execInstallScript(installScriptPath)
+}
+
+func getPkgName(args []string) (string, error) {
+	if len(args) == 0 {
+		return "", errors.New("package name required")
+	}
+
+	return args[0], nil
+}
+
+func getInstallScriptPaht(pkgName string) (string, error) {
+	// TODO: パスを決め打ちしないでconfigで持つ
+	installScriptPath, err := filepath.Abs(fmt.Sprintf("./%s/install.sh", pkgName))
+	if err != nil {
+		return "", err
+	}
+
+	return installScriptPath, nil
+}
+
+func execInstallScript(installScriptPath string) error {
+	o, err := exec.Command("/bin/sh", installScriptPath).Output()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(o))
+
 	return nil
 }
