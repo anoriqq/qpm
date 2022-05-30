95e5f430b4a5d94d3fc8b4959cfc827616fa5694
anoriqq <shota.yoshikawa@anoriqq.com>
2022-05-31 00:18:04 +0900

    feat: add version command (#8)

---
+++ internal/cmd/version.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show version info",
+	RunE:  versionRun,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+const version = "v0.0.2"
+
+func versionRun(_ *cobra.Command, _ []string) error {
+	fmt.Println(version)
+	return nil
+}
